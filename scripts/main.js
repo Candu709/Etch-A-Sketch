a383d6c36fe259a3d5f0ab17571b91539549f822
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2023-02-25 15:23:23 +0100

    Add pen drawing mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,19 @@
 const sheet = document.querySelector(`.sketchGrid`)
 
+let pointerIsDown = false
+document.addEventListener (`pointerdown`, () => pointerIsDown = true)
+document.addEventListener (`pointerup`, () => pointerIsDown = false)
+
 function drawGrid(size) {
   for (let y = size; y > 0; y--) {
     for (let x = 1; x <= size; x++) {
       const pixel = document.createElement(`div`);
       pixel.classList.add(`pixel`, `y=${y}`, `x=${x}`);
       pixel.style.setProperty(`--size`, size)
+      pixel.addEventListener(`pointerdown`, () => pixel.classList.add (`colored`))
+      pixel.addEventListener (`pointerenter`, () => {
+      if (pointerIsDown) pixel.classList.add (`colored`)
+      })
       sheet.appendChild(pixel);
     };
   }
