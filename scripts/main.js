d776f4321aa7bda3ebe3c5bf6aa1a6f1e6356c2b
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2023-03-11 21:43:21 +0100

    Reformat using the Prettier extension
    See: https://prettier.io/

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-const sheet = document.querySelector(`.sketchGrid`)
+const sheet = document.querySelector(`.sketchGrid`);
 
 function drawGrid(size) {
   for (let y = size; y > 0; y--) {
     for (let x = 1; x <= size; x++) {
       const pixel = document.createElement(`div`);
       pixel.classList.add(`pixel`, `y=${y}`, `x=${x}`);
-      pixel.style.setProperty(`--size`, size)
+      pixel.style.setProperty(`--size`, size);
       sheet.appendChild(pixel);
-    };
+    }
   }
 }
 
-drawGrid(16)
+drawGrid(16);
